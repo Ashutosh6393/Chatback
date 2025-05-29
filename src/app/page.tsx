6f0b62400d483aab6439a6230f51c5ca5274a064
Ashutosh Verma <vashutosh625@gmail.com>
2025-05-30 02:25:58 +0530

    add navbar and hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
 // import Image from "next/image";
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
-  return <div />
+  return (
+    <main>
+      <div className="container">
+        <div className="flex min-h-[720px] flex-col items-center justify-center">
+          <Button
+            variant={'outline'}
+            className="mb-10 cursor-pointer rounded-full px-12 text-md shadow-none hover:bg-background"
+          >
+            See How It Works
+          </Button>
+          <h1 className="bg-gradient-to-br from-zinc-800 to-zinc-700 bg-clip-text text-center font-black text-7xl text-transparent leading-20 ">
+            Upload. Train. Chat.
+            <br />
+            It’s That Simple.
+          </h1>
+          <p className="mt-10 text-center font-semibold text-xl">
+            Create custom AI chatbots trained on your FAQs, product docs,
+            <br />
+            internal wikis, and more — in minutes.
+          </p>
+          <Button className="mt-10 cursor-pointer rounded-lg bg-zinc-900 p-5 font-md text-lg text-white shadow-grad">
+            Buid your agent
+          </Button>
+        </div>
+      </div>
+    </main>
+  )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,37 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image.js'
 import Link from 'next/link.js'
 
 const Navbar = () => {
   return (
-    <header>
-      <div className="container fixed top-0 z-50 mx-auto flex justify-between bg-white/50 px-4 backdrop-blur-md">
+    <header className="fixed top-0 w-full backdrop-blur-md">
+      <div className="container flex items-center justify-between py-4 ">
         <div className="flex items-center justify-between">
           <Link href="/" className="flex items-center gap-2">
-            <Image src="/logo.png" alt="ChatBack" width={100} height={100} />
-            <h1 className="font-bold text-2xl">ChatBack</h1>
+            <Image
+              src="/logo.png"
+              alt="Chatback"
+              width={30}
+              height={30}
+              className="rounded-lg"
+            />
+            <h1 className="font-bold text-xl">Chatback</h1>
           </Link>
         </div>
 
-        <nav className="flex items-center gap-4">
+        <nav className="flex items-center justify-between gap-14 font-semibold text-md tracking-wide">
           <Link href="/">Features </Link>
           <Link href="/">Pricing</Link>
           <Link href="/">Contact</Link>
         </nav>
-        <div className="flex items-center gap-2" />
+        <div className="flex items-center gap-2">
+          <Button variant="ghost" className="cursor-pointer font-bold text-md">
+            Sign in
+          </Button>
+          <Button className="cursor-pointer bg-zinc-900 font-bold text-md text-white">
+            Try it for free
+          </Button>
+        </div>
       </div>
     </header>
   )
